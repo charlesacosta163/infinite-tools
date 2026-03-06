7772bbc73e298da407d807fd585b4b018c960f55
Charles Acosta <charlesacosta163@gmail.com>
2026-03-06 00:52:41 -0500

    hotfix 1

--- lib/tools-data.ts
+++ lib/tools-data.ts
@@ -634,7 +634,7 @@ export const toolsData = [
         name: "TOD Calculator",
         creator: "Jaxx and DotWallop",
         description: "The TOD (Top of Descent) Calculator is a tool helps Infinite Flight pilots easily calculate their Top of Descent distance and recommended vertical speed for a smooth descent. Based on Infinite Flight's tutorial",
-        imageUrl: "https://cdn.shopify.com/s/files/1/2773/1296/files/Infographic_of_Top_of_Descent_-_Pilot_Mall.png?v=1688074614",
+        imageUrl: "https://global.discourse-cdn.com/infiniteflight/original/4X/b/0/5/b0580ff604f2b451212967b868b8677544b69971.jpeg",
         tags: ["FPL", "utility"],
         link: "https://stunt-magic-29211896.figma.site/",
     },
